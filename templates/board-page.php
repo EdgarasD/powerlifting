c29f348ce19830c33933213b6774ff7b75a81bc6
Edgaras <edgaras.dimsa@gmail.com>
2018-07-12 19:54:25 +0300

    boardpage

--- templates/board-page.php
+++ templates/board-page.php
@@ -4,7 +4,7 @@
             <div class="col-12 col-sm-4">
                 <div class="blogtext">
                     <h3>Garbės lenta</h3>
-                    <p>one of the best and cleaniest wordpress theme for agencies and online portfolios</p>
+                    <!-- <p>one of the best and cleaniest wordpress theme for agencies and online portfolios</p> -->
                     <a class="carousel-control-prev left myleft" href="#carouselExample" role="button" data-slide="prev">
                         <span class="fa fa-angle-left" aria-hidden="true">
                         </span>
@@ -21,51 +21,60 @@
                     <div class="carousel-inner">
                         <div class="carousel-item active">
                             <div class="row">
-                                <div class="col-6 last">
+                                <div class="col-3 last">
                                     <a href="blog.html">
-                                        <img src="assets/images/img16.jpg" alt="table" class="w-100">
+                                        <img src="<?php echo ASSETS_URL ?>images/pupinis.jpg" alt="Paulius Pupinis" class="w-100">
                                     </a>
-                                    <p>some of the best furniture designs of the year 1027</p>
-                                    <a class="ourblogbtn" href="blog.html">
+                                    <p>Paulius Pupinis</p>
+                                    <!-- <a class="ourblogbtn" href="blog.html">
                                         +more
-                                    </a>
+                                    </a> -->
                                 </div>
-                                <div class="col-6 last1">
+                                <div class="col-3 last1">
                                     <a href="blog.html">
-                                        <img src="assets/images/img17.jpg" alt="bench" class="w-100">
+                                        <img src="<?php echo ASSETS_URL ?>images/egidijus.png" alt="Egidijus Valčiukas" class="w-100">
                                     </a>
-                                    <p>some of the best furniture designs of the year 1027</p>
-                                    <a class="ourblogbtn" href="blog.html">
+                                    <p>Egidijus Valčiukas</p>
+                                    <!-- <a class="ourblogbtn" href="blog.html">
                                         +more
+                                    </a> -->
+                                </div>
+                                <div class="col-3 last1">
+                                    <a href="blog.html">
+                                        <img src="<?php echo ASSETS_URL ?>images/milda.png" alt="Milda Brūverė" class="w-100">
                                     </a>
+                                    <p>Milda Brūverė</p>
+                                    <!-- <a class="ourblogbtn" href="blog.html">
+                                        +more
+                                    </a> -->
                                 </div>
                             </div>
                         </div>
 
                         <div class="carousel-item">
                             <div class="row">
-                                <div class="col-6 last">
+                                <div class="col-3 last">
                                     <a href="blog.html">
-                                        <img src="assets/images/carousel1.jpg" alt="table" class="w-100">
+                                        <img src="<?php echo ASSETS_URL ?>images/Klaudijus.png" alt="Klaudijus Malevskis" class="w-100">
                                     </a>
-                                    <p>some of the best furniture designs of the year 1027</p>
-                                    <a class="ourblogbtn" href="blog.html">
+                                    <p>Klaudijus Malevskis</p>
+                                    <!-- <a class="ourblogbtn" href="blog.html">
                                         +more
-                                    </a>
+                                    </a> -->
                                 </div>
-                                <div class="col-6 last1">
+                                <div class="col-3 last1">
                                     <a href="blog.html">
-                                        <img src="assets/images/carousel2.jpg" alt="bench" class="w-100">
+                                        <img src="<?php echo ASSETS_URL ?>images/daiva.png" alt="Daiva Žarovienė" class="w-100">
                                     </a>
-                                    <p>some of the best furniture designs of the year 1027</p>
-                                    <a class="ourblogbtn" href="blog.html">
+                                    <p>Daiva Žarovienė</p>
+                                    <!-- <a class="ourblogbtn" href="blog.html">
                                         +more
-                                    </a>
+                                    </a> -->
                                 </div>
                             </div>
                         </div>
 
-                        <div class="carousel-item">
+                        <!-- <div class="carousel-item">
                             <div class="row">
                                 <div class="col-6 last">
                                     <a href="blog.html">
@@ -87,7 +96,7 @@
                                     </a>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
