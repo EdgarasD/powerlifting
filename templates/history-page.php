8ef709f260070192cbf3d32be41ff2ff9e8eb7df
Edgaras <edgaras.dimsa@gmail.com>
2018-07-17 19:35:36 +0300

    changes

---
+++ templates/history-page.php
@@ -0,0 +1,61 @@
+<section class="story">
+
+    <div class="lines">
+
+    </div>
+
+    <div data-aos="fade-right" data-aos-easing="linear" data-aos-duration="500" class="ourstory">
+        <div class="inner">
+            <h3>ISTORIJA</h3>
+        </div>
+
+    </div>
+
+
+    <div class="ourpara">
+        <div data-aos="fade-left" data-aos-easing="linear" data-aos-duration="500" class="inner1">
+            <p>LIETUVOJE SSRS okupacijos metais LJTF buvo atletinės gimnastikos dalis,
+              bet buvo rengiamos neoficialios jėgos varžybos ir čempionatai Šiauliuose,
+              Klaipėdoje, Taline, Tiumenėje. Labai populiarūs buvo Ventos taurės
+              turnyrai Kuršėnuose. 1979 04 04 Šiauliuose įvyko I neoficialus
+              Lietuvos jėgos dvikovės (iki 1982 dvikovė: pritūpimas ir spaudimas)
+              čempionatas, jie vyko kasmet iki 1987. Iki 1990,
+              Lietuvos nepriklausomybės atkūrimo metų, priklausė Lietuvos atletinės
+              gimnastikos federacijai, 1987-91 Lietuvos susivienijimas veikė
+              kaip atskiras Lietuvos atletinės gimnastikos federacijos padalinys
+              (vad. E. Kryževičius). 1991 09 22 įvyko Lietuvos jėgos trikovės federacijos (LJTF)
+              steigiamasis susirinkimas, 1993 06 05 Kaune – steigiamoji konferencija,
+              nuo 1994 LJTF yra Europos jėgos trikovės federacijos (EPF) narė.
+              1989-96 vyko tarptautiniai Baltijos taurės turnyrai (1992 įvyko Šiauliuose,
+              pirmasis Lietuvoje, jame dalyvavo Lietuvos, Latvijos, Lenkijos, Norvegijos,
+              Suomijos, Švedijos ir Vokietijos komandos); 1989 Lietuva užėmė 7 v.,
+              1991 – 5 v., 1992 – 3 v., 1993 – 4 v., 1994 – 7 v. (1995 ir 1996 nedalyvavo).
+              1992 Baltijos čempionu tapo A. Kirkliauskas (per 95 kg), 1993 – G. Buitkus (67,5 kg).
+              1997 Palangoje surengtas X Europos veteranų čempionatas. Lietuvoje kasmet rengiami jaunių,
+              jaunimo, suaugusiųjų ir veteranų čempionatai, atskiri štangos spaudimo
+              gulint ant suolelio čempionatai. Žymesnieji Lietuvos jėgos trikovininkai
+              (rezultatai pateikti lentelėse): A. Abrutis, A. Gečas (abu Mažeikių Švyturys; tr. R. ir P. Miliai),
+              E. Paplauskas (Kuršėnų Žėrutis, tr. V. Augustauskas), Ž. Savickas (Biržų Jėga, tr. S. Česnauskas),
+              S. Kondratenko (Rokiškio Grizlis, tr. A. Kondratenko),
+              D. Žarovienė (2006 Europos veteranų čempionė, 2007 pasaulio veteranų iki 60 kg sv. kategorijos ir absoliučioji čempionė;
+              Kauno jėgos sporto centras, tr. R. Linkauskas), A. Navickas (Marijampolė, tr. A. Kirkliauskas),
+              V. Blekaitis, D. Juodgudis, L. Palivonas, A. Vosylius, T. Pupelis, S. Lasys,
+              A. Jašmontas – Lietuvos rinktinės nariai, Lietuvos čempionai ir rekordininkai,
+              įvykdę elito klasės normatyvą, A. Paulauskas, A. Velička, T. Pupelis
+              (visi Kauno jėgos sporto centras, tr. R. Linkauskas), A. Virbičianskas,
+              A. Naglius (Joniškio VARŪNA, tr.A. Virbičianskas ).
+              Kultivuojama Akmenėje, Alytuje, Biržuose, Birštone, Gargžduose, Kaune, Klaipėdoje,
+              Kuršėnuose, Marijampolėje, Mažeikiuose, Panevėžyje, Radviliškyje, Rokiškyje,
+              Šiauliuose, Švenčionyse, Ventoje, Vilniuje, Utenoje, Joniškyje, Jonavoje,
+              Lazdijuose. Pajėgiausi šalies jėgos trikovės klubai – Mažeikių Švyturys,
+              Marijampolės Suvalkijos ąžuolas, Šiaulių Uola, Kuršėnų Žėrutis, Rokiškio Grizlis,
+              Kauno jėgos sporto centras. Jėgos trikovės specialistus rengia LKKA.
+              2008 Lietuvos jėgos trikovės federacijai priklausė 53 klubai,
+              juose sportavo > 2300 jėgos trikovininkų, juos treniravo 37 treneriai.
+
+            </p>
+        </div>
+
+    </div>
+
+</section>
---
+++ templates/news-page.php
@@ -0,0 +1,98 @@
+<section class="latest">
+    <div data-aos="fade-up" data-aos-easing="linear" data-aos-duration="500" class="lathead">
+        <h3>Federacijos naujienos</h3>
+        <!-- <P>Lorem Ipsum is simply dummy text of the printing and typeset-ting industry. Lorem Ipsum has been the industry's...</P> -->
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div data-aos="fade-up" data-aos-duration="500" class="col-12 col-sm-6 col-md-4">
+                <div class="lat">
+                    <a href="productsingle.html">
+                        <figure>
+                            <img src="<?php echo ASSETS_URL ?>images/img7.jpg" alt="Naujiena">
+                        </figure>
+                        <span>2018-07-06</span>
+                        <a href="productsingle.html">
+                        </a>
+                        <h6 class="text-uppercase">Sveikiname veteranus su puikiu startu Europos čempionate</h6>
+                    </a>
+                </div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="800" class="col-12 col-sm-6 col-md-4">
+                <div class="lat">
+                    <a href="productsingle.html">
+                        <figure>
+                            <img src="<?php echo ASSETS_URL ?>images/img8.jpg" alt="Naujiena">
+                        </figure>
+                        <span>Time</span>
+                        <a href="productsingle.html">
+                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
+                        </a>
+                        <h6 class="text-uppercase">Aprašymas</h6>
+                    </a>
+                </div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1100" class="col-12 col-sm-6 col-md-4">
+                <div class="lat">
+                    <a href="productsingle.html">
+                        <figure>
+                            <img src="<?php echo ASSETS_URL ?>images/img9.jpg" alt="Naujiena">
+                        </figure>
+                        <span>Time</span>
+                        <a href="productsingle.html">
+                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
+                        </a>
+                        <h6 class="text-uppercase">Aprašymas</h6>
+                    </a>
+                </div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="500" class="col-12 col-sm-6 col-md-4">
+                <div class="lat">
+                    <a href="productsingle.html">
+                        <figure>
+                            <img src="<?php echo ASSETS_URL ?>images/img10.jpg" alt="Naujiena">
+                        </figure>
+                        <span>Time</span>
+                        <a href="productsingle.html">
+                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
+                        </a>
+                        <h6 class="text-uppercase">Aprašymas</h6>
+                    </a>
+                </div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="800" class="col-12 col-sm-6 col-md-4">
+                <div class="lat">
+                    <a href="productsingle.html">
+                        <figure>
+                            <img src="<?php echo ASSETS_URL ?>images/img11.jpg" alt="Naujiena">
+                        </figure>
+                        <span>Time</span>
+                        <a href="productsingle.html">
+                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
+                        </a>
+                        <h6 class="text-uppercase">Aprašymas</h6>
+                    </a>
+                </div>
+            </div>
+            <div data-aos="fade-up" data-aos-duration="1100" class="col-12 col-sm-6 col-md-4">
+                <div class="lat">
+                    <a href="productsingle.html">
+                        <figure>
+                            <img src="<?php echo ASSETS_URL ?>images/img12.jpg" alt="Naujiena">
+                        </figure>
+                        <span>Time</span>
+                        <a href="productsingle.html">
+                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
+                        </a>
+                        <h6 class="text-uppercase">Aprašymas</h6>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="latestbtn" href="#">
+        Daugiau naujienų
+    </a>
+</section>
