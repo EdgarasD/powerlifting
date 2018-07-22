fba0217f0be851570057c5132b46255bc33bf0d9
Antanas <antanas.gecas@gmail.com>
2018-07-22 18:53:54 +0300

    demo about

--- page.php
+++ page.php
@@ -1,4 +1,16 @@
 <?php get_header(); ?>
 <body>
 <?php get_template_part ('templates/top-menu', 'page'); ?>
+
+
+<div class="container">
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <br>
+        <div>
+          <?php the_content(); ?>
+        </div>
+ <?php endwhile; endif; ?>
+</div>
+
+
 <?php get_footer(); ?>
