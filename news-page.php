af0a69276b46580245bca39b03a6fbab651665be
Antanas <antanas.gecas@gmail.com>
2018-07-24 20:39:39 +0300

    news feed

---
+++ news-page.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+	<h1><?php the_field('heading', get_option('page_for_posts')); ?></h1>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* The loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
+
+			<?php twentythirteen_paging_nav(); ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><
+  
+<?php get_footer(); ?>
