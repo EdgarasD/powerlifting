7ab6894879567183ece9da8f1c0f366f1585518d
Antanas <antanas.gecas@gmail.com>
2018-07-31 20:45:17 +0300

    add news posts

--- templates/news-page.php
+++ templates/news-page.php
@@ -6,108 +6,59 @@
 
 
     <div class="container">
+        <div class="row">
 
       <?php
+$first_six = 0;
 $query = new WP_Query(array('orderby' => 'post_date', 'order' => 'DESC')); //Orders posts by post_date in descending order
 
 // The Loop
-if ($query->have_posts()) {
-    echo '<div class="day-posts">';
-    while ($query->have_posts()) {
-        $query->the_post();
-        var_dump(the_content());
-        the_date('l jS F Y', '<div class="title">', '</div>');
-        the_title('<div class="title">', '</div>');
-    }
-    echo '</div>';
-}
+if ($query->have_posts()):
+
+    while ($query->have_posts()&& $first_six<6):?>
+      <?php $query->the_post();
+      $first_six++;?>
+
+      <div data-aos="fade-up" data-aos-duration="500" class="col-12 col-sm-6 col-md-4">
+          <div class="lat">
+              <a href="
+              <?php echo get_permalink(); ?>
+              ">
+                  <figure>
+                      <img src="
+                      <?php
+                      if (get_field('nuotrauka')!=null){
+                        $pic =  the_field('nuotrauka');
+                        $picture = wp_get_attachment_image_src($pic, 'news_image')[0];
+                        echo $picture;
+                    //    var_dump ($picture);
+                      }
+                      else{echo 'wp-content/themes/powerlifting/assets/images/img8.jpg';}
+                      ?>" alt="<?php the_title(); ?>" height="300px" width="300px">
+                  </figure>
+                  <span><?php echo get_the_date( 'Y-m-d' ); ?></span>
+
+                  <h6 class="text-uppercase"><?php  the_title(); ?></h6>
+              </a>
+          </div>
+      </div>
+
+
+<?php  endwhile;
+endif;
 ?>
-        <div class="row">
-            <div data-aos="fade-up" data-aos-duration="500" class="col-12 col-sm-6 col-md-4">
-                <div class="lat">
-                    <a href="productsingle.html">
-                        <figure>
-                            <img src="<?php echo ASSETS_URL ?>images/img7.jpg" alt="Naujiena">
-                        </figure>
-                        <span>2018-07-06</span>
-                        <a href="productsingle.html">
-                        </a>
-                        <h6 class="text-uppercase">Sveikiname veteranus su puikiu startu Europos čempionate</h6>
-                    </a>
-                </div>
-            </div>
-            <div data-aos="fade-up" data-aos-duration="800" class="col-12 col-sm-6 col-md-4">
-                <div class="lat">
-                    <a href="productsingle.html">
-                        <figure>
-                            <img src="<?php echo ASSETS_URL ?>images/img8.jpg" alt="Naujiena">
-                        </figure>
-                        <span>Time</span>
-                        <a href="productsingle.html">
-                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
-                        </a>
-                        <h6 class="text-uppercase">Aprašymas</h6>
-                    </a>
-                </div>
-            </div>
-            <div data-aos="fade-up" data-aos-duration="1100" class="col-12 col-sm-6 col-md-4">
-                <div class="lat">
-                    <a href="productsingle.html">
-                        <figure>
-                            <img src="<?php echo ASSETS_URL ?>images/img9.jpg" alt="Naujiena">
-                        </figure>
-                        <span>Time</span>
-                        <a href="productsingle.html">
-                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
-                        </a>
-                        <h6 class="text-uppercase">Aprašymas</h6>
-                    </a>
-                </div>
-            </div>
-            <div data-aos="fade-up" data-aos-duration="500" class="col-12 col-sm-6 col-md-4">
-                <div class="lat">
-                    <a href="productsingle.html">
-                        <figure>
-                            <img src="<?php echo ASSETS_URL ?>images/img10.jpg" alt="Naujiena">
-                        </figure>
-                        <span>Time</span>
-                        <a href="productsingle.html">
-                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
-                        </a>
-                        <h6 class="text-uppercase">Aprašymas</h6>
-                    </a>
-                </div>
-            </div>
-            <div data-aos="fade-up" data-aos-duration="800" class="col-12 col-sm-6 col-md-4">
-                <div class="lat">
-                    <a href="productsingle.html">
-                        <figure>
-                            <img src="<?php echo ASSETS_URL ?>images/img11.jpg" alt="Naujiena">
-                        </figure>
-                        <span>Time</span>
-                        <a href="productsingle.html">
-                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
-                        </a>
-                        <h6 class="text-uppercase">Aprašymas</h6>
-                    </a>
-                </div>
-            </div>
-            <div data-aos="fade-up" data-aos-duration="1100" class="col-12 col-sm-6 col-md-4">
-                <div class="lat">
-                    <a href="productsingle.html">
-                        <figure>
-                            <img src="<?php echo ASSETS_URL ?>images/img12.jpg" alt="Naujiena">
-                        </figure>
-                        <span>Time</span>
-                        <a href="productsingle.html">
-                            <!-- <i class="fa fa-shopping-cart" aria-hidden="true"></i> -->
-                        </a>
-                        <h6 class="text-uppercase">Aprašymas</h6>
-                    </a>
-                </div>
-            </div>
+
+
+
+
+
+
+
+
         </div>
     </div>
+
+
     <a class="latestbtn" href="#">
         Daugiau naujienų
     </a>
